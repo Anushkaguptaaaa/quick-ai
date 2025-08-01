e5b98f91e093416e3c3e363b4f0d3e07e1d40ede
Anushka Gupta <aanushkaguptaa4@gmail.com>
2025-08-02 01:47:23 +0530

    backend

---
+++ server/routes/aiRoutes.js
@@ -0,0 +1,10 @@
+import express from "express";
+import { auth } from "../middlewares/auth.js";
+import { generateArticle, generateBlogTitle, generateImage, removeImageBackground, removeImageObject, resumeReview } from "../controllers/aiController.js";
+
+
+const aiRouter = express.Router();
+
+aiRouter.post('/generate-article', auth, generateArticle)
+
+export default aiRouter
