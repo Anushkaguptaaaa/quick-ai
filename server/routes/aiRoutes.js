adb55ea0c3c638306a282dc1b81766e1cc4f994f
Anushka Gupta <aanushkaguptaa4@gmail.com>
2025-08-02 01:48:05 +0530

    second

--- server/routes/aiRoutes.js
+++ server/routes/aiRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { auth } from "../middlewares/auth.js";
-import { generateArticle, generateBlogTitle, generateImage, removeImageBackground, removeImageObject, resumeReview } from "../controllers/aiController.js";
+import { generateArticle } from "../controllers/aiController.js";
 
 
 const aiRouter = express.Router();
